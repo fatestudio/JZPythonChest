958af1006d2d20fbbb83306bf31d0410daa85038
Qin (Jason) Zhou <fatestudio@gmail.com>
2024-09-25 22:36:18 -0700

    upload 1920.py

---
+++ leetcode/1920.py
@@ -0,0 +1,10 @@
+# 1920. Build Array from Permutation
+# https://leetcode.com/problems/build-array-from-permutation/
+
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ret = []
+        for i in range(len(nums)):
+            ret.append(nums[nums[i]])
+        return ret
